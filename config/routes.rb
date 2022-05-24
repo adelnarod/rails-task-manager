bbd646c284940ff9d70dafa940624fa63a4b5df1
Adel Narod <adelnarod@gmail.com>
2022-05-24 14:49:18 +0400

    all implemented

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update'
-  delete 'restaurants/:id', to: 'tasks#destroy'
+  delete 'tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
